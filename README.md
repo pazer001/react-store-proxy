8b258669fe17ed62cafd947b1469c366844f7723
Paz Lazar <pazer@gmx.com>
2017-07-04 16:34:01 +0300

    Rebuild

--- README.md
+++ README.md
@@ -1,1 +1,100 @@
-# react-store-proxy
+# Proxer
+
+# Introduction
+Proxer is a store library for react which uses Proxy.
+
+It helps you write a general store which connect all your app to.
+
+Proxer is a very tiny library which not affect your bundle size and aim to be very fast.
+
+# Motivation
+Proxer is here to solve a problem caused by Redux and Mobx.
+
+##Redux
+The most popular store library for redux made by dan abramov.
+
+Redux is a good library but it's weakness is it's boilerplate. it takes more boilerplate and learning curve.
+
+to use redux use must control - Dispatcher / Action Creators / Constants / Reducers / Combined reducers / Connect etc...
+
+## Mobx
+A new kind of store library but its main core is that it watch for changes.
+
+This method create a little boilerplate and is efficient but not efficient as redux.
+
+## Proxer
+Proxer came to solve these problem.
+
+instead of creating lots of boilerplate, all you have to do is to create a store, use our Provider component and import your store when ever you want. that's it!
+
+Well, it seems easy like mobx but here you dont have to use decorators which affect yor bundle size, you dont have to use observables which affects your performance and also you can create as many stores you like.
+
+# How To Use
+First of all install the library:
+
+npm install react-store-proxy --save
+Now, lets say you have this store object:
+
+```javascript
+const initialState   =  {
+  todos: [{
+    text: 'Eat food',
+    completed: true
+  }, {
+    text: 'Exercise',
+    completed: false
+  }]
+}
+```
+To use it, all we have to do is to import createStore function and export its result
+
+import {createStore} from 'react-store-proxy';
+
+```javascript
+const initialState   =  {
+  todos: [{
+    text: 'Eat food',
+    completed: true
+  }, {
+    text: 'Exercise',
+    completed: false
+  }]
+}
+
+const store    = createStore(initialState);
+export default store;
+```
+Now, go to your root App component and wrap it with our Provider component like that.
+
+```javascript
+import React, { Component } from 'react';
+import {Provider} from 'react-store-proxy';
+import store from './store';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider bind={this}>
+      <div className="App">
+        <button onClick={() => store.todos.push({text: 'My new todo', completed: false})}>click here</button>
+        <p className="App-intro">
+          {store.todos.map((todo, key) =>
+            <li key={key}>{todo.text}</li>
+          )}
+        </p>
+      </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
+```
+
+Thats it!
+
+for this simple usage, all you have to do when you want to add you new todo is just to create you action and push to its array.
+
+it doesnt care if you use some async action. the only thing matter is that you assign a new value to you store.
+
+And if you bwant you can build as many stores as you like and architect youre actions where ever you want.
--- index.js
+++ index.js
@@ -1,88 +1,76 @@
 import React from 'react';
-import {isEqual} from 'lodash';
+import {isEqual, cloneDeep} from 'lodash';
 
-let providerRoot  = null;
+export default class Proxer extends React.Component {
+    static providerRoot;
 
-const render = (object = null, key = null, value = null) => {
-    providerRoot.setState({});
-}
-
-const proxyToObject = object => {
-  if(!object) return undefined;
-  let flatObject  = {};
-  for(let key in object) {
-    switch (typeof object[key]) {
-      case 'object':
-        flatObject[key]   = proxyToObject(object[key])
-        break;
-      default:
-        flatObject[key]   = object[key];
-    }
-  }
-  return flatObject;
-}
-
-const proxyHandler  = {
-  get: (object, key) => key in object ? object[key] : object,
-  set: (object, key, value, receiver) => {
-    if(object.length !== undefined) {
-      object[key]   = value;
-      render(object, key, value);
-      return true;
+    static render(object = null, key = null, value = null, middleware = null) {
+        if (middleware && typeof middleware === 'function') middleware(cloneDeep(object), key, cloneDeep(value));
+        Proxer.providerRoot.setState({});
     }
 
-    switch (typeof value) {
-      case 'object':
-        if(!isEqual(object[key], value)) {
-          object[key]   = createStore(value)
-          render(object, key, value)
+    static createStore(object = {}, config = {}) {
+        for (let key in object) {
+            if (typeof object[key] === 'object') object[key] = this.createStore(object[key]);
         }
-        break;
-      default:
-      if(object[key] !== value) {
-        object[key]   = value;
-        render(object, key, value)
-      }
+
+        const proxyHandler = {
+            get: (object, key) => key in object ? object[key] : object,
+            set: (object, key, value) => {
+                if (object.length !== undefined) {
+                    object[key] = value;
+                    this.render(object, key, value, config.middleware);
+                    return true;
+                }
+
+                switch (typeof value) {
+                    case 'object':
+                        if (!isEqual(object[key], value)) {
+                            object[key] = this.createStore(value)
+                            this.render(object, key, value, config.middleware)
+                        }
+                        break;
+                    default:
+                        if (object[key] !== value) {
+                            object[key] = value;
+                            this.render(object, key, value, config.middleware)
+                        }
+                }
+                return true
+            },
+            delete: (object, key) => {
+                if (key in object) {
+                    delete object[key];
+                    this.render(object, key, null, config.middleware);
+                    return true
+                } else {
+                    return false
+                }
+            }
+        };
+
+
+        const store = new Proxy(object, proxyHandler);
+        if (config.toWindow && config.name && typeof window === 'object') {
+            if (!window.stores) window.stores = {};
+            if (!window.stores[config.name]) {
+                window.stores[config.name] = store;
+                window.getStore = this.getStore;
+            }
+        }
+        return store;
     }
-    return true
-  },
-  delete: (object, key) => {
-    if(key in object) {
-      delete object[key];
-      render(object, key);
-      return true
-    } else {
-      return false
+
+    static getStore(object) {
+        let flatObject = cloneDeep(object);
+        return flatObject;
     }
-  }
-}
 
-export const createStore = (object = {}, config = {}) => {
-  for(let key in object) {
-    if(typeof object[key] === 'object') object[key] = createStore(object[key]);
-  }
-  const store   =  new Proxy(object, proxyHandler);
-  if(config.toWindow && config.name && typeof window === 'object') {
-    if(!window.stores) window.stores   = {};
-    if(!window.stores[config.name]) {
-      window.stores[config.name]   = store;
-      window.getStore   = getStore;
+    componentWillMount() {
+        Proxer.providerRoot     =   this.props.bind;
     }
-  }
-  return store;
-}
 
-export const getStore = object => {
-  let flatObject  = proxyToObject(object);
-  return flatObject;
-}
-
-export class Provider extends React.Component {
-  componentWillMount() {
-    providerRoot  = this.props.bind;
-  }
-
-  render() {
-    return this.props.children
-  }
+    render() {
+        return this.props.children
+    }
 }
